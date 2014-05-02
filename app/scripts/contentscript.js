6e82255b45c623447596a38164de21b587d4de7d
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2014-05-02 13:33:57 +0100

    Whew.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -50,47 +50,48 @@ console.log('\'Allo \'Allo! Content script');
 				}
 			]
 
+		// put data into pie chart
+
+			// Pie chart settings
+
+			var options = {
+				//Boolean - Whether we should show a stroke on each segment
+				segmentShowStroke : true,
+
+				//String - The colour of each segment stroke
+				segmentStrokeColor : "#fff",
+
+				//Number - The width of each segment stroke
+				segmentStrokeWidth : 2,
+
+				//The percentage of the chart that we cut out of the middle.
+				percentageInnerCutout : 20,
+
+				//Boolean - Whether we should animate the chart
+				animation : true,
+
+				//Number - Amount of animation steps
+				animationSteps : 100,
+
+				//String - Animation easing effect
+				animationEasing : "easeOutBounce",
+
+				//Boolean - Whether we animate the rotation of the Doughnut
+				animateRotate : true,
+
+				//Boolean - Whether we animate scaling the Doughnut from the centre
+				animateScale : false,
+
+				//Function - Will fire on animation completion.
+				onAnimationComplete : null
+			}
+
 		// insert chart
 
 			//Get the context of the canvas element we want to select
-			var ctx = document.getElementById("myChart").getContext("2d");
+			var ctx = document.getElementById('myChart').getContext('2d');
 			new Chart(ctx).Doughnut(chartData,options);
 
-			// put data into pie chart
-
-				// Pie chart settings
-
-				var options = {
-					//Boolean - Whether we should show a stroke on each segment
-					segmentShowStroke : true,
-
-					//String - The colour of each segment stroke
-					segmentStrokeColor : "#fff",
-
-					//Number - The width of each segment stroke
-					segmentStrokeWidth : 2,
-
-					//The percentage of the chart that we cut out of the middle.
-					percentageInnerCutout : 20,
-
-					//Boolean - Whether we should animate the chart
-					animation : true,
-
-					//Number - Amount of animation steps
-					animationSteps : 100,
-
-					//String - Animation easing effect
-					animationEasing : "easeOutBounce",
-
-					//Boolean - Whether we animate the rotation of the Doughnut
-					animateRotate : true,
-
-					//Boolean - Whether we animate scaling the Doughnut from the centre
-					animateScale : false,
-
-					//Function - Will fire on animation completion.
-					onAnimationComplete : null
-				}
 
 			// find topic strings in body copy
 			// wrap strings in <attr> tag
